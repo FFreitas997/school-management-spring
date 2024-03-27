9f3d5228c6a5fafd4720d724fe0af7c319a9c0a7
FFreitas <francisco.freitas.ff@gmail.com>
2024-03-27 13:07:02 +0000

    javadoc creation banner

--- src/main/java/org/example/schoolmanagementsystemspring/SchoolManagementSystemSpringApplication.java
+++ src/main/java/org/example/schoolmanagementsystemspring/SchoolManagementSystemSpringApplication.java
@@ -3,6 +3,9 @@ package org.example.schoolmanagementsystemspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Create a Spring Banner for your application <a href="http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20">here</a>
+ */
 @SpringBootApplication
 public class SchoolManagementSystemSpringApplication {
 
