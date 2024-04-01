274db423c67573b0a5d8e449f59859d403e90708
FFreitas <francisco.freitas.ff@gmail.com>
2024-04-01 18:33:53 +0100

    authentication process init with filter & jwt service & UserDetailsService creation

---
+++ src/main/java/org/example/schoolmanagementsystemspring/auth/AuthenticationFilter.java
@@ -0,0 +1,29 @@
+package org.example.schoolmanagementsystemspring.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * @author FFreitas
+ * @LinkedIn: <a href="https://www.linkedin.com/in/francisco-freitas-a289b91b3/">Francisco Freitas</a>
+ * @Github: <a href="https://github.com/FFreitas997">FFreitas997</a>
+ * @Project: School-Management-System-Spring
+ */
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest req,
+            @NonNull HttpServletResponse res,
+            @NonNull FilterChain chain
+    ) throws ServletException, IOException {
+        chain.doFilter(req, res);
+    }
+}
---
+++ src/main/java/org/example/schoolmanagementsystemspring/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package org.example.schoolmanagementsystemspring.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.example.schoolmanagementsystemspring.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author FFreitas
+ * @LinkedIn: <a href="https://www.linkedin.com/in/francisco-freitas-a289b91b3/">Francisco Freitas</a>
+ * @Github: <a href="https://github.com/FFreitas997">FFreitas997</a>
+ * @Project: School-Management-System-Spring
+ */
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
