779dd8fa0f04388fc07dd0b3ece34f6bbb9481d1
FFreitas <francisco.freitas.ff@gmail.com>
2024-04-01 18:31:53 +0100

    creation entity User & User Roles & User permission & User repository

---
+++ src/main/java/org/example/schoolmanagementsystemspring/user/Role.java
@@ -0,0 +1,52 @@
+package org.example.schoolmanagementsystemspring.user;
+
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author FFreitas
+ * @LinkedIn: <a href="https://www.linkedin.com/in/francisco-freitas-a289b91b3/">Francisco Freitas</a>
+ * @Github: <a href="https://github.com/FFreitas997">FFreitas997</a>
+ * @Project: School-Management-System-Spring
+ */
+@Getter
+public enum Role {
+    ADMIN(
+            Permission.ADMIN_CREATE, Permission.ADMIN_READ,
+            Permission.ADMIN_UPDATE, Permission.ADMIN_DELETE,
+            Permission.TEACHER_CREATE, Permission.TEACHER_READ,
+            Permission.TEACHER_UPDATE, Permission.TEACHER_DELETE,
+            Permission.STUDENT_CREATE, Permission.STUDENT_READ,
+            Permission.STUDENT_UPDATE, Permission.STUDENT_DELETE,
+            Permission.PARENT_CREATE, Permission.PARENT_READ,
+            Permission.PARENT_UPDATE, Permission.PARENT_DELETE
+    ),
+    TEACHER(
+            Permission.TEACHER_CREATE, Permission.TEACHER_READ,
+            Permission.TEACHER_UPDATE, Permission.TEACHER_DELETE
+    ),
+    STUDENT(
+            Permission.STUDENT_CREATE, Permission.STUDENT_READ,
+            Permission.STUDENT_UPDATE, Permission.STUDENT_DELETE
+    ),
+    PARENT(
+            Permission.PARENT_CREATE, Permission.PARENT_READ,
+            Permission.PARENT_UPDATE, Permission.PARENT_DELETE
+    );
+
+    private final List<Permission> permissions;
+
+    Role(Permission... permissions) { this.permissions = List.of(permissions); }
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = permissions
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getValue()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+}
---
+++ src/main/java/org/example/schoolmanagementsystemspring/user/User.java
@@ -0,0 +1,96 @@
+package org.example.schoolmanagementsystemspring.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.example.schoolmanagementsystemspring.token.Token;
+import org.example.schoolmanagementsystemspring.utils.BaseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author FFreitas
+ * @LinkedIn: <a href="https://www.linkedin.com/in/francisco-freitas-a289b91b3/">Francisco Freitas</a>
+ * @Github: <a href="https://github.com/FFreitas997">FFreitas997</a>
+ * @Project: School-Management-System-Spring
+ */
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity implements UserDetails {
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "picture")
+    private String picture;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+    @Column(name = "locked")
+    private boolean isLocked;
+
+    @Column(name = "enabled")
+    private boolean isEnabled;
+
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() { return email; }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return expirationDate.isAfter(LocalDateTime.now());
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !isLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
