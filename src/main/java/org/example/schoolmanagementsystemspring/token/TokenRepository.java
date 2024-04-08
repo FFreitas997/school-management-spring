ddb19c72efacb6bee5f16cf204bf009033c396cb
FFreitas <francisco.freitas.ff@gmail.com>
2024-04-08 18:54:28 +0100

    fix schedule

--- src/main/java/org/example/schoolmanagementsystemspring/token/TokenRepository.java
+++ src/main/java/org/example/schoolmanagementsystemspring/token/TokenRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,6 +31,6 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
     List<Token> findAllValidTokensByUserId(Integer userID);
 
     @Modifying
-    @Query("delete from Token token where token.expired = true and token.revoked = true")
-    void deleteExpiredTokens();
+    @Transactional
+    void deleteByExpiredTrueAndRevokedTrue();
 }
