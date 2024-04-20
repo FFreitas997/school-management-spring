7a41c1926fcac845c4c1988e2ab26882b57873de
FFreitas <francisco.freitas.ff@gmail.com>
2024-04-20 15:27:58 +0100

    simple test && github actions changed

--- src/test/java/org/example/schoolmanagementsystemspring/SchoolManagementSystemSpringApplicationTests.java
+++ src/test/java/org/example/schoolmanagementsystemspring/SchoolManagementSystemSpringApplicationTests.java
@@ -1,13 +1,26 @@
 package org.example.schoolmanagementsystemspring;
 
+import org.example.schoolmanagementsystemspring.user.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SchoolManagementSystemSpringApplicationTests {
 
     @Test
     void contextLoads() {
+        // create a simple test
+        User user1 = new User();
+        user1.setId(10);
+        User user2 = new User();
+        user2.setId(10);
+        assertEquals(user2.getId(), user1.getId());
     }
+
+    //method to return biggest integer
+
+
 
 }
