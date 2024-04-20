e99808df799941b7752b3b9bdc0727ff8fa8a5a5
FFreitas <francisco.freitas.ff@gmail.com>
2024-04-20 18:15:07 +0100

    unused import fix

--- src/main/java/org/example/schoolmanagementsystemspring/attachment/util/AttachmentUtility.java
+++ src/main/java/org/example/schoolmanagementsystemspring/attachment/util/AttachmentUtility.java
@@ -6,7 +6,6 @@ import org.example.schoolmanagementsystemspring.user.entity.User;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 /**
  * @author FFreitas
