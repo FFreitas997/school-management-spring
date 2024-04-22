5cefa7002daccfc301e4c47ff8c1e4855ecdadc7
FFreitas <francisco.freitas.ff@gmail.com>
2024-04-22 18:37:49 +0100

    some changes

--- src/test/java/org/example/schoolmanagementsystemspring/user/repository/UserRepositoryTest.java
+++ src/test/java/org/example/schoolmanagementsystemspring/user/repository/UserRepositoryTest.java
@@ -1,11 +1,12 @@
 package org.example.schoolmanagementsystemspring.user.repository;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,31 +19,39 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * <a href="https://github.com/FFreitas997/">Github</a>
  */
 
+
 @Testcontainers
-@DataJdbcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+/*properties = {
+        "spring.datasource.url=jdbc:postgresql://localhost:5432/database",
+        "spring.datasource.username=username",
+        "spring.datasource.password=password",
+        "spring.datasource.driver-class-name=org.postgresql.Driver"
+}*/
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Container
-    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:latest");
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:latest")
+            .withUsername("username")
+            .withDatabaseName("database")
+            .withPassword("password")
+            .withExposedPorts(5432);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.driver-class-name", container::getDriverClassName);
+    }
 
     @Autowired
     UserRepository userRepository;
-
-    @BeforeAll
-    static void beforeAll() {
-        container.start();
-    }
 
     @Test
     void connectionTest() {
         assertTrue(container.isCreated());
         assertTrue(container.isRunning());
-        assertTrue(container.isHealthy());
-    }
-
-    @AfterAll
-    static void afterAll() {
-        container.stop();
     }
 }
